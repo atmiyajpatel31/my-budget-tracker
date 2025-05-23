f3aaed4ca1b8b9b483f5615fee0678ef14a74130
atmiyajpatel31 <75593467+atmiyajpatel31@users.noreply.github.com>
2025-05-22 23:43:43 -0400

    Add files via upload

---
+++ my-budget-tracker-github-ready/src/LogTab.js
@@ -0,0 +1,5 @@
+import React from "react";
+function LogTab() {
+  return <div><h2>Income/Expense Log</h2><p>Form to add entries will go here.</p></div>;
+}
+export default LogTab;
---
+++ my-budget-tracker-github-ready/src/MappingTab.js
@@ -0,0 +1,5 @@
+import React from "react";
+function MappingTab() {
+  return <div><h2>Mapping</h2><p>This is where you manage dropdown values (Years, Categories, etc.)</p></div>;
+}
+export default MappingTab;
