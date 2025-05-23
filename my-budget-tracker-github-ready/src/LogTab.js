3c0a46bce65b851c9258fe72254aec729ad0ef3b
atmiyajpatel31 <75593467+atmiyajpatel31@users.noreply.github.com>
2025-05-22 23:45:06 -0400

    Delete my-budget-tracker-github-ready directory

--- my-budget-tracker-github-ready/src/LogTab.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function LogTab() {
-  return <div><h2>Income/Expense Log</h2><p>Form to add entries will go here.</p></div>;
-}
-export default LogTab;
--- my-budget-tracker-github-ready/src/TrackerTab.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function TrackerTab() {
-  return <div><h2>Monthly Tracker</h2><p>Summary view of expenses and income.</p></div>;
-}
-export default TrackerTab;
