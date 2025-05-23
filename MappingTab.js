a1ab16c5349760adbbe940bf7e3df98706e058fe
atmiyajpatel31 <75593467+atmiyajpatel31@users.noreply.github.com>
2025-05-22 23:50:11 -0400

    Delete MappingTab.js

--- MappingTab.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function MappingTab() {
-  return <div><h2>Mapping</h2><p>This is where you manage dropdown values (Years, Categories, etc.)</p></div>;
-}
-export default MappingTab;
